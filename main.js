ad0a2e900ff8058bfa8cd30025801e57713f3bc4
anubittech <anubit.dev@gmail.com>
2024-07-11 10:36:49 +0530

    in main.js file Chart write below toggle func because its synchronise lang overwrite the toggle function & not able to toggle

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const sidebar = document.querySelector(".sidebar")
-const toogle = document.querySelector('#wrapper .navbar .navbar_content .sidebar_toggle')
+const toogle = document.querySelector(' .sidebar_toggle')
 const SidebarItem = document.querySelector(".sidebar_item")
 const MainContent = document.querySelector('.content')
 const Link = document.querySelectorAll('.link')
 const canvasBar = document.getElementById('canvas-bar')
 const canvasLine = document.getElementById('canvas-line')
 // Link.forEach.style.color = 'red'
+
 function toggleSidebar(){
   if(sidebar.style.width === '260px'){
     sidebar.style.width = '0px';
     SidebarItem.style.opacity = "0";
-    // console.log("click 1");
+    console.log("click 1");
   }else{
     sidebar.style.width = "260px";
     SidebarItem.style.opacity = "1";
 
-    // console.log("click 2");
+    console.log("click 2");
   }
 }
 
@@ -28,6 +29,9 @@ function CloseSidebar(){
   }
 }
 
+MainContent.addEventListener('click',CloseSidebar)
+toogle.addEventListener('click',toggleSidebar)
+// Chart here
 new Chart(canvasBar, {
   type: 'bar',
   data: {
@@ -47,7 +51,6 @@ new Chart(canvasBar, {
   }
 });
 
-
 new Chart(canvasLine,{
   type:"line",
   data:{
@@ -62,6 +65,3 @@ new Chart(canvasLine,{
     }]
   }
 })
-
-MainContent.addEventListener('click',CloseSidebar)
-toogle.addEventListener('click',toggleSidebar)
